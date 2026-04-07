13145c66d81ac81ec3bf8f0b82678223d70bed0b
Joshua Morrison <joshua.morrison@colorado.edu>
2026-04-07 23:44:03 +0000

    ...

--- app.py
+++ app.py
@@ -3,13 +3,32 @@ import psycopg2
 
 app = Flask(__name__)
 
-# @app.route('/')
-# def hello_world():
-#    return 'Hello, World!'
+URL = "postgresql://my_postgresql_6c81_user:EBU4t7ahZIXXQPdDtwy2BgJBvYF9X6zG@dpg-d7a70ufpm1nc73bvaqsg-a/my_postgresql_6c81"
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
 
 
 @app. route('/db_test')
 def testing():
-    conn = psycopg2.connect("postgresql://my_postgresql_6c81_user:EBU4t7ahZIXXQPdDtwy2BgJBvYF9X6zG@dpg-d7a70ufpm1nc73bvaqsg-a/my_postgresql_6c81")
+    conn = psycopg2.connect(URL)
     conn. close()
     return "Database Connection Successful"
+
+@app. route ('/db_create')
+def creating():
+    conn = psycopg2.connect(URL)
+    cur = conn. cursor()
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS Basketball(
+            First VARCHAR(255),
+            Last VARCHAR(255),
+            City VARCHAR(255),
+            Name VARCHAR(255),
+            Number INT
+            );
+    ''')
+    conn.commit ()
+    conn. close ()
+    return "Basketball Table Successfully Created"
